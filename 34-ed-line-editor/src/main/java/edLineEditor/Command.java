f9d6b6cf69ceb8a6c00e64d79a95de2838aef8cf
ReignW <wangzhoutao188@icloud.com>
2018-06-07 15:09:24 +0800

    asd

--- 34-ed-line-editor/src/main/java/edLineEditor/Command.java
+++ 34-ed-line-editor/src/main/java/edLineEditor/Command.java
@@ -99,8 +99,8 @@ class CommandZ implements  Command{
     CommandZ(int[] pins,EditTool e,String p){
         this.Pins=pins;
         if(pins[0]==-1){
-            Pins[0]=1;
-            Pins[1]=e.h.getMax();
+            Pins[0]=e.getPinpoint();
+            Pins[1]=e.getPinpoint();
         }
 
         this.edit=e;
